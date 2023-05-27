dcdff841f9ec98716beb71547af728cf710b8c5c
Max <maxedin97@gmail.com>
2023-05-28 00:20:56 +0200

    added example.py

--- example.py
+++ example.py
@@ -1,40 +1,50 @@
 import matplotlib.pyplot as plt
 from chrom_modelling import model
 
-# Define components
-salt = Component(
+# Define components (e.g., salts, biomolecules, additives)
+salt = model.Component(
     type = "salt",
 
 )
-protein1 = Component()
-protein2 = Component()
 
-components = [salt, protein1, protein2]
+# Define solutions (e.g., load solutions, buffers)
+load = model.Solution(
+    components=[salt],
+    concentrations=[0.4] # [M]
+)
+eluent = model.Solution(
+    components=[salt],
+    concentrations=[0]
+)
 
+# Define model units (e.g., columns, tubings, tanks)
+column = model.Unit(
+    type="LRMP", # e.g., Lumped-Rate Model with Pores
+    nz=50, # Axial discretization [-]
+    lz=0.2, # Axial length [m]
+    lr=0.05, # Radial length [m]
+    components=[salt],
+    init_concentrations=[0.4] # [M]
+)
 
-# Define solutions
-buffer1 = Solution(components, [0.2, 1.8, 0.4])
-buffer2 = Solution(components, [0.2, 0., 0.])
-
-
-# Define system
-column = Unit(type="LRMP", length=0.30, Dax=0.0001, ax_disc=50, components=components)
-column.set_init_concentrations([0.2, 0.0, 0.0])
-
-
-# Add phases
-phase1 = Phase(inlet_solution=buffer1,
-               flowrate=0.00001,
-               length=0.5)
-phase2 = Phase(inlet_solution=buffer2,
-               flowrate=0.00001,
-               length=10)
+# Define experiment phases (e.g., loading phase, elution phase)
+injection_phase = model.Phase(
+    inlet_solution=load,
+    flowrate=0.001, # [L/s]
+    v_end=0.01 # [L]
+)
+elution_phase = model.Phase(
+    inlet_solution=eluent,
+    flowrate=0.001,
+    v_end=0.1
+)
 
 
 # Create experiment
-experiment = Experiment()
-experiment.units = [column]
-experiment.components=components
-experiment.add_phase(phase1)
-experiment.add_phase(phase2)
+experiment = model.Experiment(
+    components=[salt],
+    units=[column],
+    phases=[injection_phase, elution_phase]
+)
+
 experiment.run()
